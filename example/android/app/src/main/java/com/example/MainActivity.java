7ce7fc09581801c03a009f5b81b250f71812b07d
franrios <fcojriosbello@gmail.com>
2016-09-08 12:25:43 +0200

    builded project

--- example/android/app/src/main/java/com/example/MainActivity.java
+++ example/android/app/src/main/java/com/example/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import com.facebook.react.ReactActivity;
+import com.brentvatne.react.ReactVideoPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainActivity extends ReactActivity {
 
