004703d5e932861b11da0f449b1f1e6685eeff83
Allan Brown <allan@thisislevelup.com>
2017-09-22 14:36:27 +0100

    Fixed issue with RN 0.44, List @Override depricated

--- android/app/src/main/java/BridgePackage.java
+++ android/app/src/main/java/BridgePackage.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 public class BridgePackage implements ReactPackage {
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
