9bcaf4b2365acd68e91deb4f6c2b0164ac646940
Kamil Moskała <91079590+moskalakamil@users.noreply.github.com>
2024-11-27 16:38:47 +0100

    Release V0.15 🎉 (#196)
    * feat: rewrite architecture (#189)
    
    * Update README.md
    
    * docs: update gif (#192)
    
    * perf: update svgs to imgs (#193)
    
    * chore: update dependencies (#194)
    
    * docs: installation step (#195)

---
+++ example/src/App.tsx
@@ -0,0 +1,75 @@
+import { useRef } from 'react';
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
+import VideoPlayer, { type VideoPlayerRef } from 'react-native-video-player';
+
+const App = () => {
+  const playerRef = useRef<VideoPlayerRef>(null);
+  const progress = useRef(0);
+
+  return (
+    <SafeAreaView>
+      <Text style={{ fontSize: 22, marginTop: 22 }}>
+        React Native Video Player
+      </Text>
+      <VideoPlayer
+        endWithThumbnail
+        thumbnail={{
+          uri: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg',
+        }}
+        // pauseOnPress={true}
+        // autoplay={true}
+        // repeat={true}
+        source={{
+          uri: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+        }}
+        renderLoader={() => (
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'black',
+              justifyContent: 'center',
+            }}
+          >
+            <ActivityIndicator size={'large'} />
+          </View>
+        )}
+        controlsTimeout={2000}
+        onProgress={({ currentTime }) => {
+          progress.current = currentTime;
+        }}
+        onError={(e) => console.log(e)}
+        showDuration={true}
+        ref={playerRef}
+      />
+      <View style={styles.btnContainer}>
+        <Button onPress={() => playerRef.current?.stop()} title="Stop" />
+        <Button onPress={() => playerRef.current?.pause()} title="Pause" />
+        <Button onPress={() => playerRef.current?.resume()} title="Resume" />
+        <Button
+          onPress={() => playerRef.current?.seek(progress.current + 10)}
+          title="Seek"
+        />
+        <Button
+          onPress={() => playerRef.current?.setFullScreen(true)}
+          title="Open fullscreen"
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  btnContainer: {
+    gap: 10,
+    margin: 20,
+  },
+});
